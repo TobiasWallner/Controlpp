8897d522856f8b30d879b92aaaf719f9f49c893d
Tobias Wallner <tobias.wallner1@gmx.net>
2025-06-08 17:44:55 +0200

    new start, this time using the Eigen3 library

--- Matrix/Matrix.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "MatrixTraits.hpp"
-#include "MatrixType.hpp"
-#include "MatrixRef.hpp"
-#include "MatrixFunctions.hpp"
--- Vector/Vector.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "VectorTraits.hpp"
-#include "VectorType.hpp"
-#include "VectorRef.hpp"
-#include "VectorFunctions.hpp"
