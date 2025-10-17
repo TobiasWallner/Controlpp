26648c68c16a1ffbc02cf6cfd1a97c2da4e135a3
Tobias Wallner <tobias.wallner1@gmx.net>
2025-10-17 15:10:24 +0200

    DssFilter now constructible from continuous transfer functions

--- include/controlpp/DiscreteFilter.hpp
+++ include/controlpp/DiscreteFilter.hpp
@@ -2,15 +2,20 @@
 
 #include <ostream>
 
+#include "ContinuousTransferFunction.hpp"
+#include "ContinuousStateSpace.hpp"
 #include "DiscreteTransferFunction.hpp"
 #include "DiscreteStateSpace.hpp"
+#include "transformations.hpp"
 
 namespace controlpp{
+
+    
 
     /**
      * \brief Controller from a discrete state space
      */
-    template<class T, int NStates_, int NInputs_, int NOutputs_>
+    template<class T, int NStates_, int NInputs_=1, int NOutputs_=1>
     class DssFilter{
         private:
         
@@ -29,8 +34,26 @@ namespace controlpp{
          * A filter will also internally track 
          */
         DssFilter(const DiscreteStateSpace<T, NStates_, NInputs_, NOutputs_>& dss)
-            : dss_(dss)
-        {}
+            : dss_(dss){}
+
+        template<int N>
+        DssFilter(const DiscreteTransferFunction<T, N, NStates_>& dtf)
+            : DssFilter(to_state_space(dtf)){}
+
+        DssFilter(
+            const ContinuousStateSpace<T, NStates_, NInputs_, NOutputs_>& css, 
+            double Ts, 
+            EDiscretisation method = EDiscretisation::tustin
+        )
+            : DssFilter(discretise(css, Ts, method)){}
+
+        template<int N>
+        DssFilter(
+            const ContinuousTransferFunction<T, N, NStates_>& ctf,
+            double Ts,
+            EDiscretisation method = EDiscretisation::tustin
+        )
+            : DssFilter(to_state_space(ctf), Ts, method){}
 
         Eigen::Vector<T, NOutputs_> input(const Eigen::Vector<T, NInputs_>& u){
             const auto [x, y] = this->dss_.eval(this->states_, u);
@@ -104,7 +127,7 @@ namespace controlpp{
      * \brief Controller from a discrete transfer function
      */
     template<class T, int NumOrder, int DenOrder>
-    class DftFilter{
+    class DtfFilter{
         private:
         
         Eigen::Vector<T, NumOrder> uk_ = Eigen::Vector<T, NumOrder>::Zero();
@@ -117,7 +140,7 @@ namespace controlpp{
          * 
          * A filter will also internally track 
          */
-        DftFilter(const DiscreteTransferFunction<T, NumOrder, DenOrder>& dtf)
+        DtfFilter(const DiscreteTransferFunction<T, NumOrder, DenOrder>& dtf)
             : dtf_(dtf)
         {}
 
