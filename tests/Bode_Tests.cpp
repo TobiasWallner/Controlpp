d6af9af9e9e3388386d5a1487aa1a9cc68aa1e76
Tobias Wallner <tobias.wallner1@gmx.net>
2025-12-06 18:34:13 +0100

    some comments and documentation

--- tests/Bode_Tests.cpp
+++ tests/Bode_Tests.cpp
@@ -8,22 +8,22 @@
     Tests if one can perform loop shaping from measured data
 */
 TEST(Bode, Calculation){
-    const auto s = controlpp::tf::s<double>;
+    const controlpp::ContinuousTransferFunction s = controlpp::tf::s<double>;
 
-    auto Gs = 1 / (1 + s + s * s);
-    auto Gb = controlpp::bode(Gs);
+    controlpp::ContinuousTransferFunction Gs = 1 / (1 + s + s * s);
+    controlpp::Bode Gb = controlpp::bode(Gs);
     std::ofstream("bode_G.csv") << Gb;
 
-    auto Rs = 0.5 + 2 / s;
+    controlpp::ContinuousTransferFunction Rs = 0.5 + 2 / s;
     
-    auto L = Gb * Rs;
+    controlpp::Bode L = Gb * Rs;
     std::ofstream("bode_L.csv") << L; // test by comparison
 
-    auto Try = L / (1 + L);
+    controlpp::Bode Try = L / (1 + L);
     std::ofstream("bode_Try.csv") << Try; // test by comparison
 
-    auto Tdy = 1 / (1 + L);
+    controlpp::Bode Tdy = 1 / (1 + L);
     std::ofstream("bode_Tdy.csv") << Tdy; // test by comparison
 
-    
+
 }
