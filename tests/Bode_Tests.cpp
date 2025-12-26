5c514f02b6dc706ad901bd11136123f0bf17fa93
Tobias Wallner <tobias.wallner1@gmx.net>
2025-12-26 12:50:49 +0100

    using new  method instead of operator<<

--- tests/Bode_Tests.cpp
+++ tests/Bode_Tests.cpp
@@ -14,31 +14,31 @@ TEST(Bode, Calculation){
 
     controlpp::ContinuousTransferFunction Gs = 1 / (1 + s + s * s);
     controlpp::Bode Gb = controlpp::bode(Gs);
-    std::ofstream("bode_G.csv") << Gb;
+    controlpp::write_csv(std::ofstream("bode_G.csv"), Gb);
 
     std::ofstream("step_G.csv") << controlpp::step(Gs);
 
     controlpp::ContinuousTransferFunction Rs = 0.5 + 2 / s;
     
     controlpp::Bode L = Gb * Rs;
-    std::ofstream("bode_L.csv") << L; // test by comparison
+    controlpp::write_csv(std::ofstream("bode_L.csv"), L); // test by comparison
 
     controlpp::Bode L2 = (1 + L);
-    std::ofstream("bode_L2.csv") << L2; // test by comparison
+    controlpp::write_csv(std::ofstream("bode_L2.csv"), L2); // test by comparison
 
     controlpp::Bode Try = L / (1 + L);
-    std::ofstream("bode_Try.csv") << Try; // test by comparison
+    controlpp::write_csv(std::ofstream("bode_Try.csv"),Try); // test by comparison
 
     controlpp::Bode Tdy = 1 / (1 + L);
-    std::ofstream("bode_Tdy.csv") << Tdy; // test by comparison
+    controlpp::write_csv(std::ofstream("bode_Tdy.csv"), Tdy); // test by comparison
 
     controlpp::TimeSeries ImpulseResponse = controlpp::impulse(Gb);
-    std::ofstream("ImpulseResponse.csv") << ImpulseResponse;
+    controlpp::write_csv(std::ofstream("ImpulseResponse.csv"), ImpulseResponse);
 
-    std::ofstream("G_1_s.csv") << (Gb * (1/s));
+    controlpp::write_csv(std::ofstream("G_1_s.csv"), (Gb * (1/s)));
 
     controlpp::TimeSeries StepResponse = controlpp::step(Gb);
-    std::ofstream("StepResponse.csv") << StepResponse;
+    controlpp::write_csv(std::ofstream("StepResponse.csv"), StepResponse);
 }
 
 TEST(Bode, set_magnitudes_dB_and_phases_deg){
